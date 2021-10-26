52ce29f0c64be5b2d90f60ef23a103ce0578d801
Kavery pm <shiny.kavery@gmail.com>
2021-10-26 08:04:10 -0300

    code cleanup

--- src/components/recomendation-list.js
+++ src/components/recomendation-list.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import RecomendationItem from "./recomendation-item";
 import classes from "./recomendation.module.css";
 const DATA = [
@@ -42,6 +42,7 @@ const DATA = [
 
     
 const RecomendationList = () => {
+    const [loadedRecomendations, setloadedRecomendations] = useState([]);
     useEffect(() => {
         fetch(
           "https://gist.githubusercontent.com/diegoacuna/0c7d5508a3af22dd1922eb98156bb402/raw/f5e87043c1e676354c9c723b0bfd23d2094a6be1/react_fullstack_recommendations_2.json")
@@ -49,12 +50,20 @@ const RecomendationList = () => {
             return response.json();
           })
           .then((data) => {
-            console.log(data);
+            const recomendations = [];
+            for (const key in data) {
+              const recomendation = {
+                id: key,
+                ...data[key],
+              };
+              recomendations.push(recomendation);
+            }
+            setloadedRecomendations(recomendations);
           });
       }, []);
   return (
     <div className={classes.list}>
-      <RecomendationItem data={DATA}></RecomendationItem>
+      <RecomendationItem data={loadedRecomendations}></RecomendationItem>
     </div>
   );
 };
