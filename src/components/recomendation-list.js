e1ab6a3a96393fb6905c536b5f8ba9dca7f1504c
Kavery pm <shiny.kavery@gmail.com>
2021-10-26 08:26:02 -0300

    added a error state

--- src/components/recomendation-list.js
+++ src/components/recomendation-list.js
@@ -5,10 +5,11 @@ import classes from "./recomendation.module.css";
 const RecomendationList = () => {
   const [loadedRecomendations, setloadedRecomendations] = useState([]);
   const [Loading, setLoading] = useState(null);
+  const [Error, setError] = useState(null);
   useEffect(() => {
     setLoading(true);
     fetch(
-      "htps://gist.githubusercontent.com/diegoacuna/0c7d5508a3af22dd1922eb98156bb402/raw/f5e87043c1e676354c9c723b0bfd23d2094a6be1/react_fullstack_recommendations_2.json"
+      "https://gist.githubusercontent.com/diegoacuna/0c7d5508a3af22dd1922eb98156bb402/raw/f5e87043c1e676354c9c723b0bfd23d2094a6be1/react_fullstack_recommendations_2.json"
     )
       .then((response) => {
         return response.json();
@@ -36,16 +37,26 @@ const RecomendationList = () => {
         setloadedRecomendations(filteredRecomendation);
       }).catch((error)=>{
         console.log(error.message);
+        setError(error.message);
       })
   }, []);
 
-  if (Loading) {
-    return <p>Loading::::</p>;
+  if(Loading && Error){
+    return<p>{Error}</p>
   }
+  if(Loading && !Error){
+    return<p>Loading</p>
+  }
+  
+ 
   return (
+    
     <div className={classes.list}>
+    
       <RecomendationItem data={loadedRecomendations}></RecomendationItem>
+      {!Error && <h1>ERROR</h1>}
     </div>
+    
   );
 };
 export default RecomendationList;
