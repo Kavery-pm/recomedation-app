9e67365b71bff132118320785e4b46a10de4048e
Kavery pm <shiny.kavery@gmail.com>
2021-10-26 01:36:32 -0300

    wrapped the list item by a wrapper component card

--- src/components/recomendation-item.js
+++ src/components/recomendation-item.js
@@ -1,13 +1,16 @@
+import Card from "../ui/Card";
 import classes from "./recomendationItem.module.css";
 const RecomendationItem = (props) => {
   return (
     <li className={classes.item}>
+        <Card>
       <div className={classes.content}>
         <h3>NAME</h3>
       </div>
       <div className={classes.content}>
         <h3>EXPERTISE</h3>
       </div>
+      </Card>
     </li>
   );
 };
