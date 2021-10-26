03a9d683a602e8150f4339b5ab33eeb7c535322d
Kavery pm <shiny.kavery@gmail.com>
2021-10-26 02:08:54 -0300

    added the recomendation item and styled them

--- src/components/recomendation-item.js
+++ src/components/recomendation-item.js
@@ -1,17 +1,47 @@
+import React from "react";
 import Card from "../ui/Card";
 import classes from "./recomendationItem.module.css";
 const RecomendationItem = (props) => {
-  return (
-    <li className={classes.item}>
-        <Card>
-      <div className={classes.content}>
-        <h3>NAME</h3>
-      </div>
-      <div className={classes.content}>
-        <h3>EXPERTISE</h3>
-      </div>
-      </Card>
-    </li>
-  );
+  const meetups = [];
+  
+  for (const key in props.data) {
+    const meetup = {
+      id: key,
+      //   source: key.source,
+      //   psychologist: key.psychologist,
+      ...props.data[key],
+    };
+    meetups.push(meetup);
+  }
+  const x = meetups.filter((a) => {
+    return a.source === "IA";
+  });
+  const y = meetups.filter((a) => {
+    return a.source === "DB";
+  });
+  const z = meetups.filter((a) => {
+    return a.source === "UA";
+  });
+ const content = [...x, ...y, ...z];
+
+return(
+    <React.Fragment>
+         {content.map((recomendation) => (
+        <li className={classes.item}>
+            <Card>
+          <div className={classes.content}>
+            <h3>{recomendation.psychologist.name}</h3>
+          </div>
+          <div className={classes.content}>
+             <h2>Expertise</h2> 
+            <h3>{recomendation.psychologist.expertise}</h3>
+          </div>
+          </Card>
+      </li>))}
+      </React.Fragment>
+      ) 
+    
+
+ 
 };
 export default RecomendationItem;
--- src/components/recomendation-list.js
+++ src/components/recomendation-list.js
@@ -2,97 +2,43 @@ import { useEffect } from "react";
 import RecomendationItem from "./recomendation-item";
 import classes from "./recomendation.module.css";
 const DATA = [
-  {
-    psychologist: {
-      id: 15,
-      name: "Joann T. Ramirez",
-      expertise: ["Depresión", "Pánico", "Sueño"],
+    {
+      "psychologist": {
+        "id": 15,
+        "name": "Joann T. Ramirez",
+        "expertise": ["Depresión", "Pánico", "Sueño"]
+      },
+      "recommendedAt": "2021-01-15",
+      "source": "DB"
     },
-    recommendedAt: "2021-01-15",
-    source: "IA",
-  },
-  {
-    psychologist: {
-      id: 17,
-      name: "Edwin D. Heiss",
-      expertise: ["Parejas", "Ansiedad", "Depresión"],
+    {
+      "psychologist": {
+        "id": 17,
+        "name": "Edwin D. Heiss",
+        "expertise": ["Parejas", "Ansiedad", "Depresión"]
+      },
+      "recommendedAt": "2021-07-05",
+      "source": "IA"
     },
-    recommendedAt: "2021-07-05",
-    source: "DB",
-  },
-  {
-    psychologist: {
-      id: 25,
-      name: "Salvador C. Milton",
-      expertise: ["TOC", "Addiciones", "Bipolaridad"],
+    {
+      "psychologist": {
+        "id": 55,
+        "name": "Todd K. Taylor",
+        "expertise": ["Bipolaridad", "Ánimo", "Sueño"]
+      },
+      "recommendedAt": "2021-11-17",
+      "source": "DB"
     },
-    recommendedAt: "2021-12-08",
-    source: "UA",
-  },
-  {
-    psychologist: {
-      id: 31,
-      name: "Tonya E. Mikesell",
-      expertise: ["Depresión", "Duelo", "Estrés"],
-    },
-    recommendedAt: "2021-06-17",
-    source: "IA",
-  },
-  {
-    psychologist: {
-      id: 55,
-      name: "Todd K. Taylor",
-      expertise: ["Bipolaridad", "Ánimo", "Sueño"],
-    },
-    recommendedAt: "2021-11-17",
-    source: "DB",
-  },
-  {
-    psychologist: {
-      id: 70,
-      name: "Kaye D. Ruiz",
-      expertise: ["Sueño", "TOC", "Alimentación"],
-    },
-    recommendedAt: "2021-04-07",
-    source: "UA",
-  },
-  {
-    psychologist: {
-      id: 71,
-      name: "Ronnie K. Manning",
-      expertise: ["Alimentación", "Ánimo", "Ansiedad"],
-    },
-    recommendedAt: "2021-12-28",
-    source: "IA",
-  },
-  {
-    psychologist: {
-      id: 75,
-      name: "Barbara J. Cloutier",
-      expertise: ["Bipolaridad", "Pánico", "Addiciones"],
-    },
-    recommendedAt: "2021-07-05",
-    source: "DB",
-  },
-  {
-    psychologist: {
-      id: 81,
-      name: "Eric J. Hanks",
-      expertise: ["Alimentación", "Infantil", "Depresión"],
-    },
-    recommendedAt: "2020-11-25",
-    source: "UA",
-  },
-  {
-    psychologist: {
-      id: 85,
-      name: "Rosemary T. Keller",
-      expertise: ["Addiciones", "Bipolaridad", "Alimentación"],
-    },
-    recommendedAt: "2021-02-04",
-    source: "IA",
-  },
-];
+    {
+      "psychologist": {
+        "id": 75,
+        "name": "Barbara J. Cloutier",
+        "expertise": ["Bipolaridad", "Pánico", "Addiciones"]
+      },
+      "recommendedAt": "2021-07-05",
+      "source": "DB"
+    }
+  ]
 const RecomendationList = () => {
   return (
     <div className={classes.list}>
